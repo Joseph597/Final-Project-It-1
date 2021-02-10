888a755a87efb8366f3f443d3c0d640e59ac1193
danie <danie@DESKTOP-EUSPEH9>
2021-02-10 17:31:49 -0500

    Committing the first file so we all have it

--- Final-It-1/src/pkgfinal/it/pkg1/FinalIt1.java
+++ Final-It-1/src/pkgfinal/it/pkg1/FinalIt1.java
@@ -12,6 +12,7 @@ public class FinalIt1 {
     public static void main(String[] args) 
     {
         System.out.println("Hello World!");
+        //adding a comment for testing purposes
     }
     
 }
